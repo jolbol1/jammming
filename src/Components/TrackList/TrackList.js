82e6b0596771892af8c2985f7ecce6bdf1ed9293
jolbol1 <github@jamesshopland.com>
2021-09-19 21:01:23 +0100

    initial attempt

---
+++ src/Components/TrackList/TrackList.js
@@ -0,0 +1,19 @@
+import React from "react";
+import "./TrackList.css";
+import { Track } from "../Track/Track";
+
+
+export class TrackList extends React.Component {
+    render() {
+        return (
+            <div className="TrackList">
+                {this.props.tracks ? this.props.tracks.map(track => {
+                    return <Track track={track} onRemove={this.props.onRemove} 
+                    onAdd={this.props.onAdd} isRemoval={this.props.isRemoval} 
+                     key={track.id} />;
+                }) : null
+                }
+            </div>
+        );
+    }
+}
